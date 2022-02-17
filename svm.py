522d33fec4efd5838d1dde630a195de96fdf90cc
anis <sinatimelapse@gmail.com>
2022-02-17 11:55:22 +0100

    now that rain and norain classes are working pretty well, I am going to introduce fog class again

--- svm.py
+++ svm.py
@@ -8,7 +8,7 @@ from sklearn.model_selection import train_test_split
 import time
 import pickle as cPickle
 
-SHAPE = (500, 500)
+SHAPE = (350, 350)
 
 def read_files(directory):
    print "Reading files..."
